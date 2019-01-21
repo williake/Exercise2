1282b6aacb52e4d932b0f48b8de383a52be13439
williake <williake@stud.ntnu.no>
2019-01-21 18:21:33 +0100

    Update foo.c

--- Part2/c/foo.c
+++ Part2/c/foo.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t lock;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    for (int j = 0; j < 1000000; j++) {
+    for (int j = 0; j < 1120000; j++) {
 	// TODO: sync access to i
+  pthread_mutex_lock(&lock);
 	i++;
+  pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
@@ -15,7 +18,9 @@ void* incrementingThreadFunction(){
 void* decrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
 	// TODO: sync access to i
+  pthread_mutex_lock(&lock);
 	i--;
+  pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
@@ -23,13 +28,13 @@ void* decrementingThreadFunction(){
 
 int main(){
     pthread_t incrementingThread, decrementingThread;
-    
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
